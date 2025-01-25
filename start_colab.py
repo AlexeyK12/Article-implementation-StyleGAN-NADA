5b457de479b1c896935ab8028c4c11ec5e14ee3a
Alexey Kudelckin <kudelkin.al@imredi.biz>
2025-01-25 14:25:01 +0300

    add start.py

---
+++ start_colab.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+colab_url = "https://colab.research.google.com/github/AlexeyK12/Article-implementation-StyleGAN-NADA/blob/main/main.ipynb"
+
+webbrowser.open(colab_url)
+
+print(f'Oткрываю блокнот в Colab: {colab_url}')
